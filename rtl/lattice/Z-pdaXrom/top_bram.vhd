c58331350fe28e7794c4fb0a785c2397c6e0afc4
pdaxrom <sashz@pdaxrom.org>
2018-05-01 02:08:09 +0800

    initial

---
+++ rtl/lattice/Z-pdaXrom/top_bram.vhd
@@ -0,0 +1,103 @@
+--
+-- Copyright (c) 2015 Marko Zec, University of Zagreb
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
+--
+-- $Id$
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+use work.f32c_pack.all;
+
+
+entity glue is
+    generic (
+	-- ISA: either ARCH_MI32 or ARCH_RV32
+	C_arch: integer := ARCH_MI32;
+	C_debug: boolean := false;
+
+	-- Main clock: 50, 62, 75, 81, 87, 100, 112, 125, 137, 150 MHz
+	C_clk_freq: integer := 48;
+
+	-- SoC configuration options
+	C_bram_size: integer := 16;
+	C_sio: integer := 1;
+	C_spi: integer := 0;
+	C_gpio: integer := 0;
+	C_simple_io: boolean := false;
+	C_timer: boolean := false
+    );
+    port (
+	clk_ext: in std_logic;
+	rs232_tx: out std_logic;
+	rs232_rx: in std_logic;
+	led: out std_logic_vector(7 downto 0);
+	btn_left, btn_right, btn_up, btn_down, btn_center: in std_logic;
+	sw: in std_logic_vector(3 downto 0)
+    );
+end glue;
+
+architecture Behavioral of glue is
+    signal clk_dvi, clk_dvin, clk_pixel, clk, rs232_break: std_logic;
+--    signal btns_n: std_logic_vector(4 downto 0);
+--    signal sw_n: std_logic_vector(3 downto 0);
+--    signal led_n: std_logic_vector(7 downto 0);
+begin
+    clkgen : entity work.clkgen
+    port map(
+	CLKI        =>  clk_ext,
+	CLKOP       =>  clk_dvi,
+	CLKOS       =>  clk_dvin,
+	CLKOS2      =>  clk_pixel,
+	CLKOS3      =>  clk
+    );
+
+    -- generic BRAM glue
+    glue_bram: entity work.glue_bram
+    generic map (
+	C_arch => C_arch,
+	C_clk_freq => C_clk_freq,
+	C_bram_size => C_bram_size,
+	C_sio => C_sio,
+	C_spi => C_spi,
+	C_gpio => C_gpio,
+	C_timer => C_timer
+    )
+    port map (
+	clk => clk,
+	sio_txd(0) => rs232_tx, sio_rxd(0) => rs232_rx,
+	sio_break(0) => rs232_break,
+	spi_sck => open, spi_ss => open, spi_mosi => open, spi_miso => open,
+	gpio => open,
+	simple_out => open,
+	simple_in => open
+    );
+	
+--    btns_n <= not (btn_left & btn_right & btn_up & btn_down & btn_center);
+--    sw_n <= not sw;
+--    led <= not led_n;
+end Behavioral;
